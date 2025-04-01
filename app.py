d9678538bac8a8f5d7a338968c3a885ec9751749
Subam Chatterjee <115694169+subhamchatterjee21@users.noreply.github.com>
2025-04-01 16:39:07 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
-os.system("pip install PyPDF2")
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
@@ -11,16 +10,16 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
-#load_dotenv()
-#os.getenv("GOOGLE_API_KEY")
-#genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 # Fetch API key from Streamlit secrets
-api_key = st.secrets["GOOGLE_API_KEY"]
+#api_key = st.secrets["GOOGLE_API_KEY"]
 
 # Configure the Generative AI API
-genai.configure(api_key=api_key)
+#genai.configure(api_key=api_key)
 
 
 
